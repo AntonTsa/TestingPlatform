aca8f4bc2a9045ac9bb6af50379c76ccdca0e0f9
anton <atsarenko23@gmail.com>
2020-03-04 18:22:05 +0200

    Created project

---
+++ src/main/java/com/gmail/atsarenko/Application.java
@@ -0,0 +1,15 @@
+package com.gmail.atsarenko;
+
+/**
+ * Main class
+ * Created on 03.03.2020 by Anton Tsarenko
+ *
+ */
+public class Application {
+
+    public static void main( String[] args ) {
+
+
+
+    }
+}
